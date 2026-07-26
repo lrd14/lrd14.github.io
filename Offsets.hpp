1052d6e9e66c9a61b210bfcbe5d0044f8b538629
noah <81028847+lrd14@users.noreply.github.com>
2026-07-26 16:10:48 +0000

    Auto update offsets

--- Offsets.hpp
+++ Offsets.hpp
@@ -1,5 +1,5 @@
 namespace Offsets {
-    inline std::string ClientVersion = "version-9affbe66b2624d20";
+    inline std::string ClientVersion = "version-ed2b47b81f08484a";
 
     namespace AirProperties {
          inline constexpr uintptr_t AirDensity = 0x18;
@@ -166,7 +166,7 @@ namespace Offsets {
     }
 
     namespace FakeDataModel {
-         inline constexpr uintptr_t Pointer = 0x85c6908;
+         inline constexpr uintptr_t Pointer = 0x7dccd88;
          inline constexpr uintptr_t RealDataModel = 0x1d0;
     }
 
@@ -344,7 +344,7 @@ namespace Offsets {
          inline constexpr uintptr_t InputObject = 0xf0;
          inline constexpr uintptr_t InputObject2 = 0x100;
          inline constexpr uintptr_t MousePosition = 0xd4;
-         inline constexpr uintptr_t SensitivityPointer = 0x0;
+         inline constexpr uintptr_t SensitivityPointer = 0x7f90688;
     }
 
     namespace ParticleEmitter {
@@ -379,7 +379,7 @@ namespace Offsets {
          inline constexpr uintptr_t NameDisplayDistance = 0x3a0;
          inline constexpr uintptr_t Team = 0x2d8;
          inline constexpr uintptr_t TeamColor = 0x3ac;
-         inline constexpr uintptr_t UserId = 0x300;
+         inline constexpr uintptr_t UserId = 0xd0;
     }
 
     namespace PlayerConfigurer {
@@ -435,8 +435,8 @@ namespace Offsets {
     }
 
     namespace RunService {
-         inline constexpr uintptr_t HeartbeatFPS = 0xd2;
-         inline constexpr uintptr_t HeartbeatTask = 0x1a0;
+         inline constexpr uintptr_t HeartbeatFPS = 0xf4;
+         inline constexpr uintptr_t HeartbeatTask = 0x24b8;
     }
 
     namespace Script {
@@ -480,7 +480,7 @@ namespace Offsets {
     }
 
     namespace SpawnLocation {
-         inline constexpr uintptr_t AllowTeamChangeOnTouch = 0x3d;
+         inline constexpr uintptr_t AllowTeamChangeOnTouch = 0x188;
          inline constexpr uintptr_t Enabled = 0x189;
          inline constexpr uintptr_t ForcefieldDuration = 0x180;
          inline constexpr uintptr_t Neutral = 0x18a;
@@ -519,7 +519,7 @@ namespace Offsets {
          inline constexpr uintptr_t JobName = 0x18;
          inline constexpr uintptr_t JobStart = 0xc8;
          inline constexpr uintptr_t MaxFPS = 0xb0;
-         inline constexpr uintptr_t Pointer = 0x815d398;
+         inline constexpr uintptr_t Pointer = 0x836aae8;
     }
 
     namespace Team {
@@ -543,10 +543,10 @@ namespace Offsets {
 
     namespace Tool {
          inline constexpr uintptr_t CanBeDropped = 0x4b8;
-         inline constexpr uintptr_t Enabled = 0x395;
+         inline constexpr uintptr_t Enabled = 0x4b9;
          inline constexpr uintptr_t Grip = 0x4ac;
          inline constexpr uintptr_t ManualActivationOnly = 0x4ba;
-         inline constexpr uintptr_t RequiresHandle = 0x245;
+         inline constexpr uintptr_t RequiresHandle = 0x4bb;
          inline constexpr uintptr_t TextureId = 0x360;
          inline constexpr uintptr_t Tooltip = 0x468;
     }
@@ -570,7 +570,7 @@ namespace Offsets {
     namespace VisualEngine {
          inline constexpr uintptr_t Dimensions = 0xab0;
          inline constexpr uintptr_t FakeDataModel = 0xa90;
-         inline constexpr uintptr_t Pointer = 0x82f16a8;
+         inline constexpr uintptr_t Pointer = 0x84f8808;
          inline constexpr uintptr_t RenderView = 0xbb8;
          inline constexpr uintptr_t ViewMatrix = 0x150;
     }
