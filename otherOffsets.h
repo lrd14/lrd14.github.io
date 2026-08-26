a2787e85df952f6fee60216d1e38a777f956c86b
noah <81028847+lrd14@users.noreply.github.com>
2026-08-26 17:03:58 +0000

    Update Roblox version and offsets in otherOffsets.h

--- otherOffsets.h
+++ otherOffsets.h
@@ -6,10 +6,10 @@
 /*                       https://gurp.cc
 /* -------------------------------------------------------------
 /*  Dumped With     : gurpDumper
-/*  Roblox Version  : version-ddf602d9cfe44005
+/*  Roblox Version  : version-17d504d2c9544583
 /*  Dumper Version  : 1.0.0
-/*  Dumped At       : 11:04 22/08/2026 (GMT)
-/*  Total Offsets   : 21
+/*  Dumped At       : 17:02 26/08/2026 (GMT)
+/*  Total Offsets   : 28
 /* -------------------------------------------------------------
 /*  Join the discord!
 /*  https://discord.gg/UZz5rxRjeM
@@ -17,25 +17,27 @@
 */
 
 namespace Offsets {
-    inline std::string ClientVersion = "version-ddf602d9cfe44005";
-    
-    namespace Instance {
-         inline constexpr uintptr_t AttributeMap = 0x38;
-         inline constexpr uintptr_t AttributeToValue = 0x18;
-    }
-     namespace AttributesMap {
-         inline constexpr uintptr_t Attributes = 0x18;
-         inline constexpr uintptr_t Length = 0x0;
-    }
-     namespace Attribute {
+    inline std::string ClientVersion = "version-17d504d2c9544583";
+
+    namespace Attribute {
          inline constexpr uintptr_t TypeIdRva = 0x867113C;
          inline constexpr uintptr_t Key = 0x0;
          inline constexpr uintptr_t Size = 0x58;
          inline constexpr uintptr_t Value = 0x8;
     }
 
+    namespace AttributesMap {
+         inline constexpr uintptr_t Attributes = 0x18;
+         inline constexpr uintptr_t Length = 0x0;
+    }
+
+    namespace Instance {
+         inline constexpr uintptr_t AttributeMap = 0x38;
+         inline constexpr uintptr_t AttributeToValue = 0x18;
+    }
+
     namespace FastClusterEntity {
-         inline constexpr uintptr_t VTableRva = 0x68CB818;
+         inline constexpr uintptr_t VTableRva = 0x6AE4C60;
          inline constexpr uintptr_t RenderQueueId = 0x10;
          inline constexpr uintptr_t TechniqueArrayPtr = 0x70;
     }
@@ -56,11 +58,11 @@ namespace Offsets {
 
     namespace WorldRoot {
          inline constexpr uintptr_t BoundFnOffset = 0x80;
-         inline constexpr uintptr_t RaycastDescriptorRva = 0x82012A0;
-         inline constexpr uintptr_t FindPartOnRayDescriptorRva = 0x8201B40;
-         inline constexpr uintptr_t FindPartOnRayWithIgnoreListDescriptorRva = 0x8201BF0;
-         inline constexpr uintptr_t FindPartOnRayWithWhitelistDescriptorRva = 0x8201CA0;
-         inline constexpr uintptr_t RaycastCachedTerrainDescriptorRva = 0x8200990;
+         inline constexpr uintptr_t RaycastDescriptorRva = 0x8024FD0;
+         inline constexpr uintptr_t FindPartOnRayDescriptorRva = 0x8025870;
+         inline constexpr uintptr_t FindPartOnRayWithIgnoreListDescriptorRva = 0x8025920;
+         inline constexpr uintptr_t FindPartOnRayWithWhitelistDescriptorRva = 0x80259D0;
+         inline constexpr uintptr_t RaycastCachedTerrainDescriptorRva = 0x80246C0;
          inline constexpr uintptr_t RaycastBatchedDescriptorRva = 0x0;
          inline constexpr uintptr_t RaycastTerrainDescriptorRva = 0x0;
     }
